ba197f42236c6c1fe00d06cf028b82cf80c86f79
Witold <witoldaleksandrowicz91@gmail.com>
2015-03-21 13:37:45 +0100

    Update Liczba parzysta.cs

--- Liczba parzysta.cs
+++ Liczba parzysta.cs
@@ -1,22 +1,44 @@
 
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
 using System.Linq;
+using System.Text;
 using System.Windows.Forms;
 
 namespace Liczba_parzysta
 {
-    static class Program
+    public partial class Form1 : Form
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
+        public Form1()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void sprawdź_Click(object sender, EventArgs e)
+        {
+            int liczba = int.Parse(Liczba.Text);
+            if (liczba % 2 == 0)
+                wynik.Text = "Liczba " + liczba + " jest liczbą parzystą";
+            else
+                wynik.Text = "Liczba " + liczba + " nie jest liczbą parzystą";
         }
     }
 }
